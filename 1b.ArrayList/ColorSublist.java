4d3d4c32fadc2d2a7d3d3b567f280cc7ca14ee0f
priyanka Jadav <154530382+Hacjadav@users.noreply.github.com>
2025-05-23 09:14:14 +0530

    Create  ColorSublist.java

---
+++ 1b.ArrayList/ColorSublist.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorSublist {
+    public static void main(String[] args) {
+        // Creating an ArrayList to store color names
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Adding different colors to the ArrayList
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Orange");
+
+        // Displaying the full list of colors
+        System.out.println("All Colors: " + colors);
+
+        // Extracting the 1st and 2nd elements using subList()
+        List<String> extractedColors = colors.subList(0, 2); // index 0 (inclusive) to 2 (exclusive)
+
+        // Displaying the extracted colors
+        System.out.println("Extracted Colors (1st and 2nd): " + extractedColors);
+    }
+}
